64f45b8abdd8259601b6e9b43db06b79e3539e21
jaroshvskii <jaroshevskiisasha@gmail.com>
2022-08-05 00:19:17 +0300

    Add task 4

---
+++ practical-work8/task4/main.js
@@ -0,0 +1,15 @@
+// Crutch.
+
+function setPageStyle() {
+  var element = document.getElementById("main");
+  element.classList.remove("mobile", "tablet");
+  
+  if (document.body.clientWidth <= 425) {
+    element.classList.add("mobile");
+  } else if (document.body.clientWidth <= 768) {
+    element.classList.add("tablet");
+  }
+}
+
+setPageStyle();
+window.addEventListener('resize', setPageStyle);
